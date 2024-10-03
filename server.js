a3cad4e535a5f532cf69cc52567372a2503e3438
teddzyb <teddybartletg88@gmail.com>
2024-10-03 10:24:26 +0800

    ref: update db name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // dbName: "sistem",
+  dbName: "sistem",
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
